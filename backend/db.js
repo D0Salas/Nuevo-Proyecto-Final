c0e93a726f4f9a56bb7ce21a3bb4eae69aa9a95d
D0Salas <156812127+D0Salas@users.noreply.github.com>
2026-02-28 15:27:48 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,16 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "PASSWORD",
-  database: "user_manager"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Error conectando a MySQL:", err);
-    return;
-  }
-  console.log("✅ MySQL conectado correctamente");
+db.connect(err => {
+  if (err) console.error(err);
+  else console.log("MySQL conectado");
 });
 
 module.exports = db;
