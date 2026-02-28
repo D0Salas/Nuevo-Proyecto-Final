2256deb44f4a8d7b2bfd33959ba2ed392b4b2910
D0Salas <156812127+D0Salas@users.noreply.github.com>
2026-02-28 15:42:01 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,13 @@ const db = require("./db");
 const path = require("path");
 
 const app = express();
+
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth"));
 app.use("/users", require("./routes/users"));
-
-app.listen(3000, ()=> console.log("API running"));
 
 app.get("/test-db", (req, res) => {
   db.query("SELECT NOW() AS time", (err, result) => {
@@ -25,4 +24,10 @@ app.get("/test-db", (req, res) => {
       database_time: result[0].time
     });
   });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
 });
