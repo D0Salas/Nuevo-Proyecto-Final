ea0b323b510cad7df79165cc435ebe1de832b5c0
D0Salas <156812127+D0Salas@users.noreply.github.com>
2026-03-01 22:01:45 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./db");
 const path = require("path");
 
 const app = express();
+
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
+
+// SERVIR FRONTEND
+app.use(express.static(path.join(__dirname,"../public")));
 
 app.use("/auth", require("./routes/auth"));
 app.use("/users", require("./routes/users"));
 
-app.listen(3000, ()=> console.log("API running"));
-
-app.get("/test-db", (req, res) => {
-  db.query("SELECT NOW() AS time", (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ status: "error" });
-    }
-
-    res.json({
-      status: "success",
-      database_time: result[0].time
-    });
-  });
-});
+app.listen(3000,()=>console.log("API running on 3000"));
